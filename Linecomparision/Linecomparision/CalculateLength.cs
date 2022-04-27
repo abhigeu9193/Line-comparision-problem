c5a29cba9b2a6fd9f31d3d2ff6a959c7adf9d4d8
Abhishek <abhigeu9193@gmail.com>
2022-04-27 15:13:46 +0530

    [line comparision]

---
+++ Linecomparision/Linecomparision/CalculateLength.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linecomparision
+{
+    internal class CalculateLength
+    {
+        public void length()
+        {
+            double x1 = 1, x2 = 2, y1 = 1, y2 = 2;
+            double length;
+            length = Math.Sqrt(x2 - x1) + Math.Pow(2, 4) + Math.Sqrt(y2 - y1) + Math.Pow(2, 4);
+
+            Console.WriteLine(length);
+        }
+    }
+}
