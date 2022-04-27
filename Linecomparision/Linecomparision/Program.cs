87b812e340710a53920d453e0d5adc588c142e97
Abhishek <abhigeu9193@gmail.com>
2022-04-27 17:42:54 +0530

    equality checked

--- Linecomparision/Linecomparision/Program.cs
+++ Linecomparision/Linecomparision/Program.cs
@@ -9,11 +9,25 @@
         length = Math.Sqrt(x2 - x1) + Math.Pow(2, 4) + Math.Sqrt(y2 - y1) + Math.Pow(2, 4);
 
         Console.WriteLine(length);
+int x = 0, y = 0;
+
+
+Random random = new Random();
+int xaxisCheck = random.Next(0, 2);
+int yaxisCheck = random.Next(0, 2);
+Console.WriteLine(random);
+if (xaxisCheck == yaxisCheck)
+
+    Console.WriteLine("true");
+
+else
+
+    Console.WriteLine("false");
 
 
 
 
 
-    
+
 
 
